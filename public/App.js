f7da775c58ac687a15a029457adea5dc48f24e28
Shakur Imperiale <56418342+MetaphorX@users.noreply.github.com>
2022-05-12 14:16:42 +0100

    Add files via upload

---
+++ public/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Chat from './pages/Chat'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
+  )
+
+}
+
+export default App
