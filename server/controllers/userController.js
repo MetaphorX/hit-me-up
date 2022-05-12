c679abe874e3e680217c04341805ba47369efdc5
Shakur Imperiale <56418342+MetaphorX@users.noreply.github.com>
2022-05-13 00:00:32 +0100

    Add files via upload

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require("../model/userModel")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 
 
 module.exports.register = async (req, res, next) =>{
@@ -11,11 +11,11 @@ module.exports.register = async (req, res, next) =>{
         const emailCheck = await User.findOne({email})
         if(emailCheck) return res.json({msg: "Email already used", status:false})
         
-        const hashPassword = await bcrypt.hash(password, 10)
+        const hashPassword = await bcrypt.hash(password, 10);
         const user = await User.create({
             email,
             username,
-            password: hashedPassword
+            password: hashPassword
         })
         delete user.password
         return res.json({status:true, user})
