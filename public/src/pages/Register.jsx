73af111f41d8abd0578f94a7fa2d84d55d0066c5
Shakur Imperiale <56418342+MetaphorX@users.noreply.github.com>
2022-05-12 14:15:58 +0100

    Add files via upload

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,17 +1,63 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import Logo from '../assets/logo.svg'
+import {ToastContainer, toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import axios from 'axios'
+import { registerRoute } from '../utils/APIRoutes'
 
 function Register() {
 
+    const [values, setValues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        cpassword:""
+    })
 
-    const handleSubmit =(event)=>{
+    const toastOptions = {
+        
+            position: "top-center",
+            autoClose:8000,
+            pauseOnHover:true,
+            draggable:true,
+            theme:"dark"
+         
+    }
+    const handleSubmit = async(event)=>{
         event.preventDefault()
-        alert("form")
+        if(handleValidation()){
+            const {password, cpassword, username, email} = values
+            const {data} = await axios.post(registerRoute, {
+                username,
+                email,
+                password
+            })
+        }
     }
 
-    const handleChange=(event)=>{}
+    const handleValidation=()=>{
+        const {password, cpassword, username, email} = values
+        if(password !== cpassword){
+            toast.error("Passwords do not match",toastOptions)
+            return false
+        }else if (username.length < 3){
+            toast.error("Username has to be more than 3 characters", toastOptions)
+            return false
+        }else if (password.length < 8){
+            toast.error("Password has to be more than than 8 characters", toastOptions)
+            return false
+        }else if (email === ""){
+            toast.error("Email is required", toastOptions)
+            return false
+        }
+
+        return true
+    }
+    const handleChange=(event)=>{
+        setValues({...values, [event.target.name]: event.target.value})
+    }
 
   return (
     <>
@@ -52,6 +98,7 @@ function Register() {
             <span>Already have an account? <Link to="/login">Login</Link></span>
         </form>
     </FormContainer>
+    <ToastContainer />
     </>
   )
 }
